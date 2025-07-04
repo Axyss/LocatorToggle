790f1cd72aa81a603fe994ae582226ea8af7968d
Axyss <alexnieto201@gmail.com>
2025-07-04 19:13:01 +0200

    add: initial setup

---
+++ src/main/kotlin/dev/axyss/locatorToggle/LocatorToggle.kt
@@ -0,0 +1,14 @@
+package dev.axyss.locatorToggle
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LocatorToggle : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
