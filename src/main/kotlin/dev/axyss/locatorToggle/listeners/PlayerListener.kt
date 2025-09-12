cc6270135026ddfb402d0a59d76497b4023301e4
Alejandro Nieto <contact@axyss.dev>
2025-09-12 19:40:34 +0200

    fix: locator bars still hidden after uninstalling (#1)

--- src/main/kotlin/dev/axyss/locatorToggle/listeners/PlayerListener.kt
+++ src/main/kotlin/dev/axyss/locatorToggle/listeners/PlayerListener.kt
@@ -1,27 +1,23 @@
 package dev.axyss.locatorToggle.listeners
 
-import dev.axyss.locatorToggle.commands.BlockDistance
+import dev.axyss.locatorToggle.LocatorBarManager
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 object PlayerListener : Listener {
     @EventHandler
     fun onPlayerJoin(event: org.bukkit.event.player.PlayerJoinEvent) {
-        val player = event.player
-        val receiveRangeAttr = player.getAttribute(org.bukkit.attribute.Attribute.WAYPOINT_RECEIVE_RANGE)
-        val transmitRangeAttr = player.getAttribute(org.bukkit.attribute.Attribute.WAYPOINT_TRANSMIT_RANGE)
-
-        receiveRangeAttr?.baseValue = BlockDistance.NONE.value
-        transmitRangeAttr?.baseValue = BlockDistance.NONE.value
+        val locatorBar = LocatorBarManager(event.player)
+        if (locatorBar.isDisabled()) {
+            locatorBar.disable(true)
+        }
     }
 
     @EventHandler
     fun onPlayerQuit(event: org.bukkit.event.player.PlayerQuitEvent) {
-        val player = event.player
-        val receiveRangeAttr = player.getAttribute(org.bukkit.attribute.Attribute.WAYPOINT_RECEIVE_RANGE)
-        val transmitRangeAttr = player.getAttribute(org.bukkit.attribute.Attribute.WAYPOINT_TRANSMIT_RANGE)
-
-        receiveRangeAttr?.baseValue = BlockDistance.WORLD_MAX.value
-        transmitRangeAttr?.baseValue = BlockDistance.WORLD_MAX.value
+        val locatorBar = LocatorBarManager(event.player)
+        if (locatorBar.isDisabled()) {
+            locatorBar.enable(true)
+        }
     }
 }
